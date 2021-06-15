3ac1f11deb31531a6fbd2e2bccac66e150122a03
laith8910 <laith8910@gmail.com>
2021-06-15 15:27:33 +0100

    Add assembly of the final message

--- main.js
+++ main.js
@@ -17,11 +17,11 @@ let data = {
  _goodAddOn : [
     'you get paid £250,000 a year',
     'you are gifted a Tesla model S',
-    'You now are the proud owner of a mansion entire filled with huskies'
+    'You now are the proud owner of a mansion entirely filled with huskies'
 ],
  _badAddOn : [
     'the car you drive is a used moped with 2 extra wheels you glued on',
-    'you are forced to wear sketchers for the rest of your life',
+    'you are forced to wear sketchers shoes for the rest of your life',
     'you make coffee at home by mixing water and dirt in a bowl'
 ],
 checkInputStyle(string){
@@ -34,10 +34,10 @@ set badAddOn(newElm){ (this._badAddOn).push(newElm); if(this.checkInputStyle(new
 set goodAddOn(newElm){ (this._goodAddOn).push(newElm); if(this.checkInputStyle(newElm)){console.log('Element added! The new array is: ' + this._goodAddOn)} else {console.log('Invalid entry, please check for README.md for help.')} },
 set goodGrade(newElm){ (this._goodGrade).push(newElm); if(this.checkInputStyle(newElm)){console.log('Element added! The new array is: ' + this._goodGrade)} else {console.log('Invalid entry, please check for README.md for help.')} },
 set badGrade(newElm){ (this._badGrade).push(newElm); if(this.checkInputStyle(newElm)){console.log('Element added! The new array is: ' + this._badGrade) } else {console.log('Invalid entry, please check for README.md for help.')}},
-get badAddOn(){ return Math.floor(Math.random() * (this._badAddOn).length)},
-get goodAddOn(){ return Math.floor(Math.random() * (this._goodAddOn).length)},
-get goodGrade(){ return Math.floor(Math.random() * (this._goodGrade).length)},
-get badGrade(){ return Math.floor(Math.random() * (this._badGrade).length)},
+get badAddOn(){ return (this._badAddOn)[Math.floor(Math.random() * (this._badAddOn).length)]},
+get goodAddOn(){ return (this._goodAddOn)[Math.floor(Math.random() * (this._goodAddOn).length)]},
+get goodGrade(){ return (this._goodGrade)[Math.floor(Math.random() * (this._goodGrade).length)]},
+get badGrade(){ return (this._badGrade)[Math.floor(Math.random() * (this._badGrade).length)]},
 }
 // create a function which takes an argument of the length of an array and generates rand index
 const findIndexLength = inArr => {
@@ -49,9 +49,15 @@ const getRandIndex = (arrIn) => {
 // Create a fucntion that when called will return the random sentence
 const runProgram = () => {
    let gradeIndex = getRandIndex(gradesArr);
+   let grade = gradesArr[gradeIndex]
    let mid = '';
    let end = '';
-   if (gradeIndex <= 2){  mid = badGrade[getRandIndex(badGrade)];
-    end = badAddOn[getRandIndex(badAddOn)]
+   if (gradeIndex <= 2) {  mid = data.goodGrade; end = data.goodAddOn }
+    else { mid = data.badGrade; end = data.badAddOn};
+    console.log('Welcome to the Chronicles of Bhasvic');
+    console.log('We wish you the best of luck...');
+    console.log(`You got grade: ${grade}, ${mid}, ${end}.`);
+
 }
-}
+
+runProgram()
