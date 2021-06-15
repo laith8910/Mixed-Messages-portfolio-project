8d1bd77b0dfa2aad5049778bd4c35f685a995d29
laith8910 <laith8910@gmail.com>
2021-06-15 14:35:51 +0100

    Put all data into a single object

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
 // Bhasvic Chronicles
 
 //write an array for each of the 3 data sets: grade, action, consequence. Use nested arrays for those with diffrent outcomes
-let gradesArr = ['A*', 'B', 'C','D','E'];
-let gradeLength = gradesArr.length -1;
-
-let goodGrade = [
+let gradesArr = ['A*', 'A' , 'B', 'C','D','E'];
+let data = {
+ _goodGrade : [
     'Neil has promoted you to be become the prinipal',
     'you are now the shadow foreign secretary... for Caroline Lucas',
     'you have been accepted into Cambridge University... doing Land Economy',
     'you have been selected to be the first passenger for starship... lets hope it works'
-];
-let badGrade = [
+],
+ _badGrade : [
     'You now work at McDonalds... and Samuel is your boss',
     'bhasvic has removed you from their list of Alumni',
     'you have now been employed as an invigilator at Bhasvic'
-];
-let goodAddOn = [
+],
+ _goodAddOn : [
     'you get paid £250,000 a year',
     'you are gifted a Tesla model S',
     'You now are the proud owner of a mansion entire filled with huskies'
-]
-let badAddOn = [
+],
+ _badAddOn : [
     'the car you drive is a used moped with 2 extra wheels you glued on',
     'you are forced to wear sketchers for the rest of your life',
     'you make coffee at home by mixing water and dirt in a bowl'
 ]
+}
 // create a function which takes an argument of the length of an array and generates rand index
+const findIndexLength = inArr => {
+    return inArr.length
+};
+const getRandIndex = (arrIn) => {
+    return Math.floor(Math.random()* findIndexLength(arrIn))
+};
+// Create a fucntion that when called will return the random sentence
+const runProgram = () => {
+   let gradeIndex = getRandIndex(gradesArr);
+   let mid = '';
+   let end = '';
+   if (gradeIndex <= 2){  mid = badGrade[getRandIndex(badGrade)];
+    end = badAddOn[getRandIndex(badAddOn)]
+}
+}
