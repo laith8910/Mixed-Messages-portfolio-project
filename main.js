87d79526a2b0873e840904bd59c07ff1d5569004
laith8910 <laith8910@gmail.com>
2021-06-15 13:48:01 +0100

    First Commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// Bhasvic Chronicles
+
+//write an array for each of the 3 data sets: grade, action, consequence. Use nested arrays for those with diffrent outcomes
+
+// create a function which takes an argument of the length of an array and generates rand index
